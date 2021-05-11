9167f52ef7271f958979ec4378bc8dfced8daf96
Gaurav <gnemade51@gmail.com>
2021-05-12 00:12:36 +0530

    Init

---
+++ test2.py
@@ -0,0 +1,6 @@
+import os
+import warnings
+warnings.filterwarnings("ignore")
+os.system('python 01_face_dataset.py')
+os.system('python 02_face_training.py')
+os.system('python 03_face_recognition.py')
